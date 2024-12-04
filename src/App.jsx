f7a57825c983b63f817ba7f9717c36d0106f578f
Liuba Kuibida <k2.liuba@gmail.com>
2024-12-04 19:10:55 +0100

    fix form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import { Button } from '@mui/material'
+import { Button, Card, CardContent, Container, TextField } from '@mui/material'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [inputValue, setInputValue] = useState('')
+  const [projectName, setprojectName] = useState('')
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setprojectName(inputValue)
+    setInputValue('')
+  }
+
   return (
     <main>
-      <Button type="submit" variant="contained">
-        Submit
-      </Button>
+      <Container maxWidth="sm">
+        <form class="form-add-prject" onSubmit={handleSubmit}>
+          <TextField
+            id="standard-basic"
+            label="Name your project"
+            variant="outlined"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <Button type="submit" variant="outlined">
+            Submit
+          </Button>
+        </form>
+      </Container>
+      <Container>
+        <Card variant="outlined" sx={{ maxWidth: 275 }}>
+          <CardContent>
+            { projectName }
+          </CardContent>
+        </Card>
+      </Container>
     </main>
   )
 }
