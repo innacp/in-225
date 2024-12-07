3480b4292023a91d27bbdd48e188f75e795958ea
innacp <inna.chapkevych@gmail.com>
2024-12-07 20:21:33 +0200

    add desc field and conditional visibility

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,31 @@ import './App.css'
 
 function App() {
   const [inputValue, setInputValue] = useState('')
-  const [projectName, setprojectName] = useState('')
+  const [inputValue2, setInputValue2] = useState('')
+  const [projectName, setProjectName] = useState('')
+  const [projectDescription, setProjectDescription] = useState('')
+  // const [show, setShow] = useState(false)
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value)
   }
+  
+  const handleInputChange2 = (event) => {
+    setInputValue2(event.target.value)    
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    setprojectName(inputValue)
+    setProjectName(inputValue)
+    setProjectDescription(inputValue2)
     setInputValue('')
+    setInputValue2('')
   }
 
   return (
     <main>
       <Container maxWidth="sm">
-        <form class="form-add-prject" onSubmit={handleSubmit}>
+        <form className="form-add-project" onSubmit={handleSubmit}>
           <TextField
             id="standard-basic"
             label="Name your project"
@@ -27,17 +36,26 @@ function App() {
             value={inputValue}
             onChange={handleInputChange}
           />
+          <TextField
+            id="standard-basic"
+            label="Add description"
+            variant="outlined"
+            value={inputValue2}
+            onChange={handleInputChange2}
+          />
           <Button type="submit" variant="outlined">
             Submit
           </Button>
         </form>
       </Container>
       <Container>
+        {(projectName  && projectDescription) &&  (
         <Card variant="outlined" sx={{ maxWidth: 275 }}>
           <CardContent>
-            { projectName }
+            {projectName + " " + projectDescription}            
           </CardContent>
         </Card>
+        )}
       </Container>
     </main>
   )
